f5dc992746bfdfbf26628e0c54241edb4297da4c
prosperityai <prosperpaul@ymail.com>
2019-01-31 17:38:12 +0300

    changes

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -6,8 +6,8 @@ const path = require('path')
 const config = require('./webpack.config.dev')
 const paths = require('./paths')
 
-const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
-const host = process.env.HOST || '0.0.0.0'
+//const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
+//const host = process.env.HOST || '0.0.0.0'
 
 module.exports = function (proxy, allowedHost) {
   return {
